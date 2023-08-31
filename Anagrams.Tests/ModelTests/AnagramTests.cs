730a0681ff32d599390b4de28a85f8f119f0e468
Aaron Brown <worldofaaron@hotmail.com>
2023-08-30 20:55:31 -0700

    pass 3 tests

--- Anagrams.Tests/ModelTests/AnagramTests.cs
+++ Anagrams.Tests/ModelTests/AnagramTests.cs
@@ -11,8 +11,30 @@ namespace Anagrams.Tests
     [TestMethod]
     public void AnagramConstructor_CreateInstanceOfAnagramChecker_AnagramChecker()
     {
-      AnagramChecker newAnagramChecker = new AnagramChecker("", new List<string> {});
+      AnagramChecker newAnagramChecker = new AnagramChecker("bat", new List<string> {"boat", "coat"});
       Assert.AreEqual(typeof(AnagramChecker), newAnagramChecker.GetType());
     }
+
+    [TestMethod]
+    public void AnagramGet_ReturnWord_String()
+    {
+      AnagramChecker newAnagramChecker = new AnagramChecker("bat", new List<string> {"boat", "coat"});
+      Assert.AreEqual("bat", newAnagramChecker.FirstWord);
+    }
+    [TestMethod]
+    public void AnagramListGet_ReturnList_List()
+    {
+      AnagramChecker newAnagramChecker = new AnagramChecker("bat", new List<string> {"boat", "coat"});
+      List<string> newList = new List<string> {"boat", "coat"};
+      CollectionAssert.AreEqual(newList, newAnagramChecker.WordList);
+    }
+
+    [TestMethod]
+    public void IsAnagram_ReturnsListOfAnagrams_List()
+    {
+      AnagramChecker newAnagramChecker = new AnagramChecker("bat", new List<string> {"boat", "coat", "tab"});
+      Assert.AreEqual(typeof(List<string>), newAnagramChecker.IsAnagram().GetType());
+    }
+
   }
 }
